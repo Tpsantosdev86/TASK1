0244b75cb9828f7e987714c3f3af87953a7a62cc
thiago pereira dos santos <cs_llanhouse@hotmail.com>
2025-09-16 00:13:06 -0300

    :card_file_box: Commit Incial

---
+++ variaveis_padrao.php
@@ -0,0 +1,40 @@
+<?php
+
+$nome_do_sistemas = "Sistema 1";
+$versao_do_sistema = "1.0.0";
+echo  $nome_do_sistemas;
+echo "<br>";
+echo " A versão do sistema é $versao_do_sistema";
+echo "<hr>";
+
+/*$pais_origem = "Brasil";*:];*/   //alteração do pais
+/*$Os_padrao = "Fedora Linux";*/ // alteração do sistema OS
+/*echo "Pais de origem $pais_origem";*/
+/*echo "Sistema Operacional $Os_padrao";*/
+
+$numero_usuarios = "100";
+echo "Numero de usuarios suportado $numero_usuarios";
+echo "<br>";
+$gestao = "Gestao de Cliente";
+echo $gestao;
+echo "<br>";
+$Control =  "Controle de Estoque";
+echo $Control;
+echo "<br>";
+$vendas = "Ponto de Venda";
+echo $vendas;
+
+// constantes
+echo "<hr>";
+define("EMISSAO", "Documentos");
+echo EMISSAO; 
+echo "<br>";
+define("ANALISE", "Relatorios");
+echo ANALISE;
+echo "<br>";
+define("AUTOMAÇÃO", "Processos Operacionais");
+echo AUTOMAÇÃO; 
+
+
+
+
